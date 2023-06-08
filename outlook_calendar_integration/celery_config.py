010f2113ef92a8e812be5bbaa92ea711a2699684
joonafinland <joona@jokivuori.net>
2023-06-08 08:02:04 +0000

    Update getting events to get events properly

--- outlook_calendar_integration/celery_config.py
+++ outlook_calendar_integration/celery_config.py
@@ -47,6 +47,7 @@ def start_watch(refresh_token, user_uuid):
     response["uuid"] = user_uuid.rstrip()
     response["google_channel"] = response_watch.get('id')
     response["google_expiry"] = int(time.mktime(expirationDateTime.timetuple()))*1000 
+    response["service"] = "outlook"
     
     response_request = requests.post(callback, data=response)
     return response
@@ -60,12 +61,13 @@ def incoming_ping(channel_id):
     user = json.loads(user_request.text)
     try:
         user_uuid = user.get("uuid", "aa")
+        email = user.get("email", "")
     except:
         return "Outlook channel not found"
     
     refresh_token = user.get("refresh", "")
     
-    formatted_changed_events = fetch_changed_events(refresh_token)
+    formatted_changed_events = fetch_changed_events(refresh_token, email)
     request_callback_ping = requests.post(os.getenv("EVENT_PING_CALLBACK_URL"), json={"channel": channel_id, "uuid": user_uuid, "tasks": formatted_changed_events})
     return f"{len(formatted_changed_events)} tasks found with at channel: {channel_id}"
 
@@ -73,7 +75,14 @@ def incoming_ping(channel_id):
 ######################
 ## HELPER FUNCTIONS ##
 ######################
+class Profile:
+    def __init__(self, calendar_token, user_email):
+        self.calendar_token = calendar_token
+        self.user = User(user_email)
 
+class User:
+    def __init__(self, email):
+        self.email = email
 
 def getToken(refresh_token):
     token_url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
@@ -119,71 +128,96 @@ def is_within_time_range(event_start, time_min, time_max):
     start_time_dt = datetime.fromisoformat(event_start.replace('Z', '+00:00')).replace(tzinfo=timezone.utc)
     return time_min <= start_time_dt <= time_max
 
+def simplify_ms_event(event, email):
+    attendees = []
 
+    if event.get("organizer", {}).get("emailAddress", {}).get("address", "") == email:
+        organizer = {"email": event.get("organizer", {}).get(
+            "emailAddress", {}).get("address", ""), "self": True}
+    else:
+        organizer = {"email": event.get("organizer", {}).get(
+            "emailAddress", {}).get("address", "")}
+    for attendee in event.get("attendees", []):
 
-def fetch_changed_events(refresh_token):
-    return []
-    client_id = os.getenv("GOOGLE_CLIENT_ID")
-    client_secret = os.getenv("GOOGLE_SECRET")
+        new_person = {'email': attendee.get("emailAddress", {}).get(
+            "address", ""), 'name': attendee.get("emailAddress", {}).get("name", "")}
+        if new_person["email"] == organizer["email"]:
+            new_person["organizer"] = True
+        if new_person["email"] == email:
+            new_person["self"] = True
+        attendees.append(new_person)
 
-    # Create the credentials object
-    creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
-        info={'client_id': client_id, 'client_secret': client_secret, 'refresh_token': refresh_token})
-
-    # Initialize the Calendar API client
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Your calendar ID
-    calendar_id = 'primary'
-
+    parsed = {'summary': event.get("subject", ""), 'id': event.get("id", ""), 'start': {'dateTime': event.get(
+        "start", {}).get("dateTime", "").split(".")[0]}, 'attendees': attendees, "organizer": organizer}
+    desc_body = event.get("body", {}).get("content", "")
     try:
-        # Get the updated events using sync token
+        try_main = event.get("onlineMeeting", {}).get("joinUrl", "")
+    except:
+        try_main = ""
+    parsed["link"] = try_main
+    if "@removed" in event:
+        parsed['status'] = 'cancelled'
+    if try_main == "":
+        link = find_link(json.loads(event))
+        parsed["link"] = link
 
-        tasks = []
-        page_token = None
-        now = datetime.now(timezone.utc)
-        time_min = now
-        time_max = now + timedelta(days=20)
+    return parsed
 
+def getMSEvent(id, refresh_token, email):
+    token, token_type = getToken(refresh_token)
+    event = requests.get(f"https://graph.microsoft.com/v1.0/me/events/{id}", headers={'Content-Type': 'application/x-www-form-url-encoded', 'Authorization': token_type + " " + token})
+    event = simplify_ms_event(json.loads(event.text), email)
+    return event
 
-        for i in range(1000):
-            print(i)
-            # Get the updated events using sync token
-            now = datetime.now(timezone.utc)
-            
-            
-            events_results = service.events().list(calendarId=calendar_id, syncToken=sync_token,
-                                                   showDeleted=True, pageToken=page_token, singleEvents=True).execute()
+def getEvents(profile):
+    token, token_type = getToken(profile.calendar_token)
+    headers={'Content-Type': 'application/x-www-form-url-encoded', 'Authorization': token_type + " " + token}
+    all_events = []
+    max_loops = 100
+    while True:
+        max_loops -= 1
+        if max_loops < 0:
+            break
 
-            for event in events_results['items']:
-                start_time = event['start'].get('dateTime', event['start'].get('date'))
-                if is_within_time_range(start_time, time_min, time_max):
-                    if 'status' in event and event['status'] == 'cancelled':
-                        tasks.append({'DELETE': [event['id']]})
-                    else:
-                        link = find_link(json.dumps(event))
-                        tasks.append({'UPDATE': [event['id'],
-                                                link,
-                                                event['start'].get('dateTime', event['start'].get('date')),
-                                                event.get('summary', ''),
-                                                event.get('organizer', {}),
-                                                [attendee['email'] for attendee in event.get('attendees', []) if "resource.calendar.google.com" not in attendee['email']]]})
-
-            # If there is a nextPageToken, update the sync_token variable and continue the loop
-            if 'nextPageToken' in events_results:
-                page_token = events_results['nextPageToken']
+        start_datetime = datetime.now().isoformat()
+        end_datetime = (datetime.now() + timedelta(days=4)).isoformat()
+        url = f"https://graph.microsoft.com/v1.0/me/calendarView/delta?startDateTime={start_datetime}&endDateTime={end_datetime}"
+        
+        res = requests.get(url,headers=headers)
+        result = json.loads(res.text)
+        
+        for event in result.get('value', []):
+            if event.get('type', '') == "seriesMaster":
+                continue
+            if event.get("type", "") == "occurrence":
+                e = getMSEvent(event.get("id",''), profile.calendar_token, profile.user.email)
             else:
-                break
+                e = simplify_ms_event(event, profile.user.email)
+            all_events.append(e)
 
+        next_page = result.get('@odata.nextLink', '')
+        if not next_page:
+            break
+    
+    return all_events
 
+def fetch_changed_events(refresh_token, email):
+    #return []
+    profile_object = Profile(refresh_token, email)
+    
+    events = getEvents(profile_object)
 
-        return tasks, events_results['nextSyncToken']
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-        return None, None
-        
-        
-        
-        
+    tasks = []
+    for event in events:
 
-
+        if 'status' in event and event['status'] == 'cancelled':
+            tasks.append({'DELETE': [event['id']]})
+        else:
+            link = find_link(json.dumps(event))
+            tasks.append({'UPDATE': [event['id'],
+                                    link,
+                                    event['start'].get('dateTime', event['start'].get('date')),
+                                    event.get('summary', ''),
+                                    event.get('organizer', {}),
+                                    [attendee['email'] for attendee in event.get('attendees', []) if "resource.calendar.google.com" not in attendee['email']]]})
+    return tasks
